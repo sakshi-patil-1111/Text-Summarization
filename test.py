4fcb9c414844ef08692657e54ac863cf76be2a93
Manpreet Kaur <preetkaurpawar8@gmail.com>
2025-04-18 14:16:48 +0530

    updated website

--- test.py
+++ test.py
@@ -7,6 +7,30 @@ import pytesseract
 import io
 import numpy as np
 import cv2
+import os
+import gdown
+import zipfile
+from transformers import BartTokenizer, BartForConditionalGeneration
+
+DRIVE_FILE_ID = "https://drive.google.com/drive/folders/1Y4FuXQoGgpYjdeJsskrYI6xmetgYr1sB?usp=sharing"
+
+
+
+def download_model_from_drive(file_id, output_dir="fine_tuned_model"):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir, exist_ok=True)
+        zip_path = "model.zip"
+        url = f"https://drive.google.com/uc?id={file_id}"
+        print("Downloading model from Google Drive...")
+        gdown.download(url, zip_path, quiet=False)
+        print("Extracting...")
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(output_dir)
+        os.remove(zip_path)
+    else:
+        return
+        
+        
 
 app = Flask(__name__)
 
@@ -17,16 +41,18 @@ model = BartForConditionalGeneration.from_pretrained("fine_tuned_model")
 # ----------------------- SUMMARIZATION FUNCTION -----------------------
 
 def generate_summary(text):
-    inputs = tokenizer([text], max_length=1024, return_tensors="pt", truncation=True)
+    download_model_from_drive(DRIVE_FILE_ID)
+    inputs = tokenizer([text], max_length=1024, return_tensors="pt", truncation=False)
     summary_ids = model.generate(
         inputs["input_ids"],
-        max_length=1024,
-        min_length=min(500, len(text) // 3),
-        length_penalty=1.0,
+        max_length=min(1024, len(text.split())),
+        min_length=min(500, len(text.split())//3),
+        length_penalty=2.0,
         num_beams=4,
         early_stopping=True,
     )
     return tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    # return len(text.split())
 
 # ----------------------- ROUTES -----------------------
 
